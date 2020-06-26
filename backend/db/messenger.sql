2952ddaaaa37ff1f17082f1abd2a72e43edc09ec
VenkateshMogili <mogilivenkatesh3@gmail.com>
2020-06-26 17:17:06 +0530

    db updated

--- backend/db/messenger.sql
+++ backend/db/messenger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 25, 2020 at 12:34 PM
+-- Generation Time: Jun 26, 2020 at 01:46 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -34,8 +34,8 @@ CREATE TABLE `conversation` (
   `image` varchar(1000) DEFAULT NULL,
   `video` varchar(1000) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'message sent time',
-  `user_id` int(11) NOT NULL COMMENT 'sender id',
-  `userTo` int(11) NOT NULL COMMENT 'receiver id',
+  `user_id` varchar(100) CHARACTER SET latin1 NOT NULL COMMENT 'sender id',
+  `userTo` varchar(100) CHARACTER SET latin1 NOT NULL COMMENT 'receiver id',
   `sent` int(11) NOT NULL DEFAULT 0 COMMENT 'sent status',
   `received` int(11) NOT NULL DEFAULT 0 COMMENT 'received status'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -43,17 +43,37 @@ CREATE TABLE `conversation` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `profiles`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(500) DEFAULT NULL,
-  `email` varchar(500) NOT NULL,
-  `password` varchar(500) NOT NULL,
-  `avatar` varchar(500) DEFAULT NULL,
-  `status` varchar(10) NOT NULL DEFAULT 'offline'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `profiles` (
+  `sno` int(11) NOT NULL,
+  `userid` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `middlename` varchar(255) DEFAULT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `relation` varchar(255) DEFAULT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'Liberty_Social/Users/logo1_c3wzeg',
+  `coverphoto` varchar(10000) NOT NULL DEFAULT 'Liberty_Social/Users/ls_cover_photo_1',
+  `gender` varchar(11) DEFAULT NULL,
+  `dob` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `habits` mediumtext DEFAULT NULL,
+  `relationstatus` varchar(255) DEFAULT NULL,
+  `interests` varchar(5000) DEFAULT NULL,
+  `religion` varchar(255) DEFAULT NULL,
+  `aboutme` varchar(5000) DEFAULT NULL,
+  `location_on_app` varchar(255) DEFAULT NULL,
+  `verified` int(11) NOT NULL DEFAULT 0,
+  `blocked` int(11) NOT NULL DEFAULT 0,
+  `appversion` int(11) DEFAULT NULL,
+  `token` varchar(1000) NOT NULL,
+  `createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedat` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -68,20 +88,21 @@ ALTER TABLE `conversation`
   ADD KEY `receiver` (`userTo`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `profiles`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `sno` (`sno`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `profiles`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `profiles`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -91,8 +112,8 @@ ALTER TABLE `users`
 -- Constraints for table `conversation`
 --
 ALTER TABLE `conversation`
-  ADD CONSTRAINT `receiver` FOREIGN KEY (`userTo`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `sender` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `receiver` FOREIGN KEY (`userTo`) REFERENCES `profiles` (`userid`),
+  ADD CONSTRAINT `sender` FOREIGN KEY (`user_id`) REFERENCES `profiles` (`userid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
